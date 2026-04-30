3f60d19e945b4dcaa21afd7b96a3057b4e2bb4bf
Quynh Nhu <hovoquynhnhu33@gmail.com>
2026-04-30 22:14:27 +0700

    Refactor code structure for improved readability and maintainability

---
+++ cartpole.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+
+env = gym.make("CartPole-v1", render_mode = "rbg_array")
+
+model = PPO("MlpPolicy", env, verbose=1, seed= 123)
+
+model.learn(total_timesteps=95000)
+
+model.save('pp0_cartpole_baseline')
+
+obs, _ = env.reset()
+for _ in range(1000):
+    action, _states = model.predict(obs, deterministic= True)
+    obs, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+        obs, _ = env.reset()
+
+env.close()
